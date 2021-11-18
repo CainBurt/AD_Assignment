9dfe6ef23858068e787a44ae8e498d6a99952234
CainBurt <cain.m.burt@gmail.com>
2021-11-18 23:18:26 +0000

    firebase authentification implemented into assignment

---
+++ main.py
@@ -0,0 +1,60 @@
+import logging 
+from flask import Flask, render_template,request 
+from google.auth.transport import requests
+import google.oauth2.id_token
+
+firebase_request_adapter = requests.Request()
+
+app = Flask(__name__) 
+
+
+@app.route('/') 
+@app.route('/index') 
+def home():
+    # Verify Firebase auth.
+    id_token = request.cookies.get("token")
+    error_message = None
+    claims = None
+
+    if id_token:
+        try:
+            # Verify the token against the Firebase Auth API. This example
+            # verifies the token on each page load. For improved performance,
+            # some applications may wish to cache results in an encrypted
+            # session store (see for instance
+            # http://flask.pocoo.org/docs/1.0/quickstart/#sessions).
+            claims = google.oauth2.id_token.verify_firebase_token(id_token, firebase_request_adapter)
+
+        except ValueError as exc:
+            # This will be raised if the token is expired or any other
+            # verification checks fail.
+            error_message = str(exc)
+
+    return render_template(
+        'index.html',
+        user_data=claims, error_message=error_message)
+
+@app.route('/login')
+def login():
+    # Verify Firebase auth.
+    id_token = request.cookies.get("token")
+    error_message = None
+    claims = None
+
+    if id_token:
+        try:
+            # Verify the token against the Firebase Auth API. This example
+            # verifies the token on each page load. For improved performance,
+            # some applications may wish to cache results in an encrypted
+            # session store (see for instance
+            # http://flask.pocoo.org/docs/1.0/quickstart/#sessions).
+            claims = google.oauth2.id_token.verify_firebase_token(id_token, firebase_request_adapter)
+
+        except ValueError as exc:
+            # This will be raised if the token is expired or any other
+            # verification checks fail.
+            error_message = str(exc)
+
+    return render_template(
+        'login.html',
+        user_data=claims, error_message=error_message)
